e6496f7dbe86d3cdbd4956d19bce0c43f3a64a40
Sang Shin <sangshinpassion@gmail.com>
2017-06-18 05:45:49 -0400

    modified by user2

--- src/main/java/mypackage/feature2.java
+++ src/main/java/mypackage/feature2.java
@@ -3,4 +3,13 @@ package mypackage;
 public class feature2 {
   // comment 1 added to feature2
   // changed feature2
+	
+	public void mymethod(){
+		
+		int myarray[] = new int[6];
+		
+		for (int i: myarray){
+			System.out.println(i);
+		}
+	}
 }
