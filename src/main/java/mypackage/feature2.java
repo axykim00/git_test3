f5244893baaa513efca1196e72a208f5e8cfaf82
Sang Shin <sangshinpassion@gmail.com>
2017-06-18 05:43:40 -0400

    added a for loop by user 1

--- src/main/java/mypackage/feature2.java
+++ src/main/java/mypackage/feature2.java
@@ -4,4 +4,14 @@ public class feature2 {
   // comment 1 added to feature2
 	// comment added from first user
   // changed feature2
+	
+	public void mymethod() {
+		
+		int myarray[] = new int[5];
+		
+		for (int i=0; i < myarray.length; i++) {
+			System.out.println(i);
+		}
+		
+	}
 }
